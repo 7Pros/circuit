dd842c8c1522b7302ba935dc4bf5b37fa1647c16
Kjell <kjellpirschel@freenet.de>
2015-05-21 19:28:12 +0200

    delete started

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import User
+from django.views.generic import DeleteView
 # Create your views here.
+
+class UserDeleteView(DeleteView):
+    model = User
+    # TODO: success_url =
