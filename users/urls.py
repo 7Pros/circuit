719796411743b9c75b11217a9d00f31878f89dc6
MetalMatze <me@metalmatze.de>
2015-05-21 19:58:40 +0200

    Create users/urls.py with comments for all urls

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+urlpatterns = [
+    # url(r'^signup/$', UsersSignup.as_view(), name='signup'),
+    # url(r'^login/$', UsersLogin.as_view(), name='signup'),
+    # url(r'^(?P<pk>\d+)/$', UsersProfile.as_view(), name='profile'),
+    # url(r'^(?P<pk>\d+)/edit/$', UsersEdit.as_view(), name='edit'),
+    # url(r'^(?P<pk>\d+)/delete/$', UsersDelete.as_view(), name='delete'),
+]
