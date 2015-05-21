093da617c1b31ca75e4d8e44eda6a85355ae4252
Gjum <gjum42@gmail.com>
2015-05-21 20:52:57 +0200

    Add some useful imports to users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import UpdateView
+from users.models import User
 
 # Create your views here.
