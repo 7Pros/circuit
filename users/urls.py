ff267609a9790a72b197cc42c9390a338ca305b9
MetalMatze <me@metalmatze.de>
2015-05-22 18:17:03 +0200

    Uncomment the url signup/

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from users.views import (
-    # UserCreateView,
+    UserCreateView,
     # UserLoginView,
     # UserProfileView,
     # UserUpdateView,
@@ -8,7 +8,7 @@ from users.views import (
 )
 
 urlpatterns = [
-    # url(r'^signup/$', UserCreateView.as_view(), name='signup'),
+    url(r'^signup/$', UserCreateView.as_view(), name='signup'),
     # url(r'^login/$', UserLoginView.as_view(), name='login'),
     # url(r'^(?P<pk>\d+)/$', UserProfileView.as_view(), name='profile'),
     # url(r'^(?P<pk>\d+)/edit/$', UserUpdateView.as_view(), name='edit'),
