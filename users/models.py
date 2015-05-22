3ae7823e92a9fb2d69b764b5d5743fdd36ff5e90
MetalMatze <me@metalmatze.de>
2015-05-22 15:48:20 +0200

    Add missing password field to User model

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(unique=True, max_length=32)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     description = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
