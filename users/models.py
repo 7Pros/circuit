5a769370a518c88edc1fe2d8f14a5bdb5fa92741
MetalMatze <me@metalmatze.de>
2015-05-22 16:41:00 +0200

    Add User model to admin.py

--- users/models.py
+++ users/models.py
@@ -9,3 +9,6 @@ class User(models.Model):
     description = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
