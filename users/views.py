b701b0fc831583affbc430c28552e0ac37ca5592
MetalMatze <me@metalmatze.de>
2015-05-26 16:53:26 +0200

    Add def form_valid() to CreateUserView to hash the user's password

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.core.urlresolvers import reverse_lazy
 
 from django.views.generic import CreateView
@@ -15,3 +16,9 @@ class UserCreateView(CreateView):
 
     # TODO change singup to `login` when merging
     success_url = reverse_lazy('signup')
+
+    def form_valid(self, form):
+        form.instance.set_password(form.cleaned_data['password'])
+        form.save()
+
+        return super(UserCreateView, self).form_valid(form)
