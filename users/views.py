95fd5574cb08cdfcc8b97a141791b642f8937268
MetalMatze <me@metalmatze.de>
2015-05-26 17:42:30 +0200

    Add first draft of sending a confirmation email after sign up

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
-from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy
-
 from django.views.generic import CreateView
 
 from users.models import User
@@ -19,6 +18,17 @@ class UserCreateView(CreateView):
 
     def form_valid(self, form):
         form.instance.set_password(form.cleaned_data['password'])
-        form.save()
+        user = form.save()
+
+        self.send_confirm_mail(user)
 
         return super(UserCreateView, self).form_valid(form)
+
+    def send_confirm_mail(self, user):
+        send_mail(
+            'Welcome to circuit',
+            'Here is the message.',
+            'hello@circuit.io',
+            [user.email],
+            fail_silently=False
+        )
