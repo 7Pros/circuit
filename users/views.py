9985c563eef268733f97df0566e08e76bdd0161b
MetalMatze <me@metalmatze.de>
2015-05-27 16:40:57 +0200

    Add UserCreateConfirmView and some improvements to UserCreateView

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
+from django.template import loader, Context
 from django.views.generic import CreateView
 
 from users.models import User
@@ -25,10 +29,29 @@ class UserCreateView(CreateView):
         return super(UserCreateView, self).form_valid(form)
 
     def send_confirm_mail(self, user):
+        template = loader.get_template('users/confirmation_email.html')
+        context = Context({'user': user})
+        html = template.render(context)
+
         send_mail(
-            'Welcome to circuit',
-            'Here is the message.',
-            'hello@circuit.io',
-            [user.email],
-            fail_silently=False
+            subject='Welcome to circuit',
+            message='hi',
+            from_email='hello@circuit.io',
+            recipient_list=[user.email],
+            fail_silently=False,
+            html_message=html
         )
+
+
+def UserCreateConfirmView(request, token):
+    try:
+        user = User.objects.get(confirm_token=token)
+
+        user.confirm_token = ''
+        user.is_active = True
+        user.save()
+
+        # TODO redirect('login')
+        return redirect('signup')
+    except ObjectDoesNotExist:
+        return redirect('signup')
