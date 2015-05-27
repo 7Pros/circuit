4a664dc1428a27d949bedf6a3569d3b4d54a7931
juan <juanecabellob@gmail.com>
2015-05-27 13:26:13 -0500

    User profile. 	-[CHANGE]   circuit/urls.py 	-[CHANGE]   templates/base.html 	-[CHANGE]   users/templates/users/user_profile.html

--- circuit/urls.py
+++ circuit/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^users/', include('users.urls'))
+    url(r'^users/', include('users.urls', namespace='users'))
 ]
