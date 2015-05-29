665be7c495dd4df6084d53c3c1f2a8e99867504f
n_righeriu <n_righeriu@campus.tu-berlin.de>
2015-05-29 16:50:52 +0200

    Added landing page

--- circuit/urls.py
+++ circuit/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from circuit import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^users/', include('users.urls', namespace='users'))
+    url(r'^users/', include('users.urls', namespace='users')),
+    url(r'^', views.landingpage, name='landingpage'),
 ]
