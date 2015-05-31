a8b8a74d72888d2a18b99fb5b1ba5d0969b1b0bb
Jelisaweta Kamm <jelisaweta_kamm@iotka.de>
2015-06-01 00:26:58 +0200

    [fix] removed print debugging lines

--- users/views.py
+++ users/views.py
@@ -83,13 +83,11 @@ class UserLoginView(generic.FormView):
     form_class = UserLoginForm
     success_url = reverse_lazy('users:profile')
     template_name = 'users/user_login_form.html'
-    print("HERE!")
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
         user = authenticate(username=username, password=password)
-        print("2HERE!")
         if user is not None and user.is_active:
             login(self.request, user)
             return super(UserLoginView, self).form_valid(form)
