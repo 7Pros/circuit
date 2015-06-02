d6fcededb5151f89e8490095c66de6c9f60f2e68
Gjum <gjum42@gmail.com>
2015-06-02 23:49:48 +0200

    Remove obsolete password edit page comment

--- users/urls.py
+++ users/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     url(r'^logout/$', views.UserLogoutView, name='logout'),
     url(r'^(?P<pk>\d+)/$', UserProfileView.as_view(), name='profile'),
     url(r'^(?P<pk>\d+)/edit/$', UserUpdateView.as_view(), name='edit'),
-    # url(r'^(?P<pk>\d+)/password/$', UserPasswordUpdateView.as_view(), name='password'),
     url(r'^(?P<pk>\d+)/delete/$', UserDeleteView.as_view(), name='delete'),
 ]
