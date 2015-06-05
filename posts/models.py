dd5614165417bd4a011212aeecd453110bf2e205
Kjell <kjellpirschel@freenet.de>
2015-06-05 18:41:03 +0200

    Add date to post model and sort posts by on profil.

--- posts/models.py
+++ posts/models.py
@@ -6,3 +6,4 @@ from users.models import User
 class Post(models.Model):
     content = models.CharField(max_length=256)
     author = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
