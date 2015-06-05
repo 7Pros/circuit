003b8003b96b407e49208ac6b7dffd15563c3db2
Gjum <gjum42@gmail.com>
2015-06-05 15:47:25 +0200

    Implement password changing

--- users/urls.py
+++ users/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^signup/confirm/(?P<token>\w+)/$', views.UserCreateConfirmView, name='signup_confirm'),
     url(r'^login/$', views.UserLoginView, name='login'),
     url(r'^logout/$', views.UserLogoutView, name='logout'),
+    url(r'^update-password/$', views.UserPasswordView, name='update-password'),
     url(r'^(?P<pk>\d+)/$', UserProfileView.as_view(), name='profile'),
     url(r'^(?P<pk>\d+)/edit/$', UserUpdateView.as_view(), name='edit'),
     url(r'^(?P<pk>\d+)/delete/$', UserDeleteView.as_view(), name='delete'),
