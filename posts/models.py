9901e60efa303038213fd315ff0a61098fc34a1e
Gjum <gjum42@gmail.com>
2015-06-07 01:32:39 +0200

    Add original post reference to post model

--- posts/models.py
+++ posts/models.py
@@ -6,4 +6,6 @@ from users.models import User
 class Post(models.Model):
     content = models.CharField(max_length=256)
     author = models.ForeignKey(User)
+    # the post of which this is a repost
+    original_post = models.ForeignKey('self', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
