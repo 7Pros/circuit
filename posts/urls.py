0da4eb6ce0f88f507109ef5aaa3b530cbb1b9c2e
Gjum <gjum42@gmail.com>
2015-06-07 01:33:28 +0200

    Add repost view and URL

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from posts import views
 urlpatterns = [
     url(r'^create/$', views.PostCreateView, name='create'),
     url(r'^(?P<pk>\d+)/$', views.PostDetailView.as_view(), name='post'),
-
+    url(r'^(?P<pk>\d+)/repost/$', views.PostRepostView, name='repost'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -13,3 +13,29 @@ def PostCreateView(request):
 class PostDetailView(generic.DetailView):
     template_name = 'posts/post_detail.html'
     model = Post
+
+
+def PostRepostView(request, pk=None):
+    user = request.user
+    original_post = Post.objects.get(pk=pk)
+    # go to first post in chain of reposts
+    while original_post.original_post:
+        original_post = original_post.original_post
+
+    if not user.is_authenticated():
+        return redirect('posts:post', pk=original_post.pk)
+
+    if user.pk == original_post.author.pk:
+        # don't repost own post
+        return redirect(request.META['HTTP_REFERER'] or 'landingpage')
+
+    existing_repost = Post.objects.get(author=user, original_post=original_post)
+    if existing_repost:
+        # don't repost twice
+        return redirect('posts:post', pk=existing_repost.pk)
+
+    repost = Post(content=original_post.content,
+                  author=user,
+                  original_post=original_post)
+    repost.save()
+    return redirect('posts:post', pk=repost.pk)
