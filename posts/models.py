4274e8451d09c46cb19d188fb52e510d83853f9a
juan <juanecabellob@gmail.com>
2015-06-07 22:41:24 +0200

    -Created the hashtag database -Post's content parsing implemented -Reference's from post implemented

--- posts/models.py
+++ posts/models.py
@@ -8,3 +8,14 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=255)
+    posts = models.ManyToManyField(Post)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import redirect
 
-from posts.models import Post
-
+from posts.models import Post, Hashtag
+import re
 
 def PostCreateView(request):
     if (len(request.POST['content']) <= 256):
+        parsedString = ParseContent(request.POST['content'])
         post = Post(content=request.POST['content'], author=request.user)
         post.save()
+        SaveHashtags(parsedString['hashtags'], post)
     return redirect(request.META['HTTP_REFERER'] or 'landingpage')
+
+
+def ParseContent(content):
+    hashtags = re.findall(r"#(\w+)", content)
+    mentions = re.findall(r"@(\w+)", content)
+    return {'hashtags': hashtags, 'mentions': mentions}
+
+def SaveHashtags(hashtags, post):
+    for hashtagWord in hashtags:
+        hashtagList = Hashtag.objects.filter(name=hashtagWord)
+
+        if len(hashtagList) == 0:
+            hashtag = Hashtag(name=hashtagWord.lower())
+            hashtag.save()
+            hashtag.posts.add(post)
+        else:
+             hashtagList[0].posts.add(post)
