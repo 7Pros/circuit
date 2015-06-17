eceea28ded1e5085cab32cf935f2825c70d3685e
Gjum <gjum42@gmail.com>
2015-06-17 21:07:50 +0200

    Add created_at and updated_at to circle model

--- circles/models.py
+++ circles/models.py
@@ -6,3 +6,5 @@ class Circle(models.Model):
     name = models.CharField(max_length=50, default='', blank=True)
     owner = models.ForeignKey(User)
     members  = models.ManyToManyField(User, related_name='members')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
