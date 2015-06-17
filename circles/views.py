cda18a50d3a472460df5a04117cf751fdedce7c1
Gjum <gjum42@gmail.com>
2015-06-17 18:46:06 +0200

    Add URLs and dummy views for circles app

--- circles/views.py
+++ circles/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+"""@package circles.views
+Circles views file.
 
-# Create your views here.
+@author 7Pros
+@copyright
+"""
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+
+class CircleList(generic.RedirectView):
+    """
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('landingpage')
+
+
+class CircleCreate(generic.RedirectView):
+    """
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('landingpage')
+
+
+class CircleEdit(generic.RedirectView):
+    """
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('landingpage')
+
+
+class CircleDelete(generic.RedirectView):
+    """
+    """
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('landingpage')
