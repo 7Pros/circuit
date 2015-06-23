2c939dc8b7b874dc7fd05e88d53c4ebf43fe4243
MetalMatze <me@metalmatze.de>
2015-06-23 19:03:39 +0200

    Add a autocomplete test to editing a circle

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,16 @@ var PostCreate = new Vue({
         content: null
     }
 });
+
+var users = [
+    { value: 'MetalMatze', data: '1' },
+    { value: 'Gjum', data: '2' },
+    { value: 'Juan', data: '3' }
+];
+
+$('.autocomplete').autocomplete({
+    lookup: users,
+    onSelect: function (suggestion) {
+        alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+    }
+});
