43c76aa159cdeebf775afff08b28d1bed810de4b
Kjell <kjellpirschel@freenet.de>
2015-06-25 16:13:51 +0200

    make post without image

--- circuit/urls.py
+++ circuit/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from circuit import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
