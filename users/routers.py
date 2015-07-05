e1ff57afc552ddf39a4c18d88d5468267950be72
juan <juanecabellob@gmail.com>
2015-07-05 18:53:26 +0200

    code improvements

--- users/routers.py
+++ users/routers.py
@@ -6,9 +6,9 @@
 """
 from swampdragon import route_handler
 from swampdragon.route_handler import ModelPubRouter
+
 from .models import Notification
 from .serializers import NotificationSerializer
-from django.contrib.auth.decorators import login_required
 
 
 class NotificationRouter(ModelPubRouter):
@@ -16,13 +16,6 @@ class NotificationRouter(ModelPubRouter):
     route_name = 'notifications'
     model = Notification
     serializer_class = NotificationSerializer
-
-    # @login_required
-    # def subscribe(self, **kwargs):
-    #     super().subscribe(**kwargs)
-    #
-    # def get_subscription_contexts(self, **kwargs):
-    #     return {'user_id': self.connection.user.pk}
 
 
 route_handler.register(NotificationRouter)
