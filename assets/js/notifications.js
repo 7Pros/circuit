da1c669bbf4d843b5ad624be109981fd61755228
juan <juanecabellob@gmail.com>
2015-07-05 19:49:50 +0200

    added notifications files

---
+++ assets/js/notifications.js
@@ -0,0 +1,115 @@
+// Ask the browser for permission to show notifications
+// Taken from https://developer.mozilla.org/en-US/docs/Web/API/Notification/Using_Web_Notifications
+window.addEventListener('load', function () {
+    Notification.requestPermission(function (status) {
+        // This allows to use Notification.permission with Chrome/Safari
+        if (Notification.permission !== status) {
+            Notification.permission = status;
+        }
+    });
+});
+
+
+// Subscribe once swampdragon is connected
+swampdragon.open(function () {
+	var userId = $("#user_id").val();
+    swampdragon.subscribe('notifications', 'notifications-'+userId);
+});
+
+// This is the list of notifications
+var notificationsList = document.getElementById("notifications");
+
+
+// New channel message received
+swampdragon.onChannelMessage(function (channels, message) {
+    if (message.action === "created") {
+        // Update badges
+        upgradeBadge();
+        // Add the notification
+        addNotification((message.data));
+    }
+});
+
+function upgradeBadge() {
+    var badgeValue = document.getElementById('badge');
+    badgeValue.innerHTML = parseInt(badgeValue.innerHTML) + 1;
+};
+
+// Add new notifications
+function addNotification(notification) {
+
+    // If we have permission to show browser notifications
+    // we can show the notifiaction
+    // TODO: fix double notifications
+    if (window.Notification && Notification.permission === "granted") {
+        new Notification(notification.message);
+    } else {
+        // TODO: personalize
+        var stack_topright = {"dir1": "up", "dir2": "right"},
+            browserNotification = new PNotify({
+                text: notification.message,
+                type: 'info',
+                styling: 'bootstrap3',
+                hide: 'true',
+                delay: '5000',
+                opacity: .6,
+                addclass: 'stack-topright',
+                stack: stack_topright
+            });
+        browserNotification.get().click(function(e){
+            if ($(e.target).is('.ui-pnotify-closer *, .ui-pnotify-sticker *')) {
+                 return;
+            }
+            window.location.replace('/users/notification/'+notification.pk+'/see/');
+        });
+    }
+
+    // Add the new notification elements
+    var buttonNotification = document.createElement("button"),
+        divRow = document.createElement("div"),
+        divStatus = document.createElement("div"),
+        spanLabel = document.createElement("span"),
+        hr = document.createElement("hr"),
+        smallCreatedAt = document.createElement("small"),
+        pCreatedAt = document.createElement("p"),
+        divContent = document.createElement("div");
+
+    //setting attributes to text content
+    divContent.setAttribute('class', 'col-xs-10');
+    divContent.innerHTML = notification.message;
+
+    //setting attributes to label
+    spanLabel.setAttribute('class', 'badge label-info');
+    spanLabel.innerHTML = '·';
+
+    //setting attributes to status label's col and adding label to it
+    divStatus.setAttribute('class', 'col-xs-1');
+    divStatus.insertBefore(spanLabel, divStatus.firstChild);
+
+    //setting attributes to row and adding content and status label
+    divRow.setAttribute('class', 'row bg-info');
+    divRow.insertBefore(divContent, divRow.firstChild);
+    divRow.insertBefore(divStatus, divRow.firstChild);
+
+    pCreatedAt.innerHTML = notification.created_at;
+    pCreatedAt.setAttribute('class', 'bg-info');
+    //adding p to small for the creation date
+    smallCreatedAt.insertBefore(pCreatedAt, smallCreatedAt.firstChild);
+
+    //setting attributes to button and adding elements
+    buttonNotification.setAttribute('class', 'list-group-item list-group-item-info');
+    buttonNotification.setAttribute('type', 'button');
+    buttonNotification.setAttribute('onclick', 'window.location.replace("/users/notification/'+notification.pk+'/see/")');
+
+    //insert everything to the right place
+    buttonNotification.insertBefore(divRow, buttonNotification.firstChild);
+
+    notificationsList.insertBefore(hr, notificationsList.childNodes[3]);
+    notificationsList.insertBefore(smallCreatedAt, notificationsList.childNodes[3]);
+    notificationsList.insertBefore(buttonNotification, notificationsList.childNodes[3]);
+
+    // Remove excess notifications
+    while (notificationsList.getElementsByTagName("button").length > 22) {
+        notificationsList.getElementsByTagName("button")[21].remove();
+    }
+}
