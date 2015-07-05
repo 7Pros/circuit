f9d2e76cf4605cb34283529eba814a62b0c043f2
juan <juanecabellob@gmail.com>
2015-07-05 18:53:37 +0200

    code improvements

--- users/serializers.py
+++ users/serializers.py
@@ -10,5 +10,4 @@ from swampdragon.serializers.model_serializer import ModelSerializer
 class NotificationSerializer(ModelSerializer):
     class Meta:
         model = 'users.Notification'
-        publish_fields = ['message', 'post', 'pk', 'created_at']
-        update_fields = ['status', 'updated_at']
+        publish_fields = ['message', 'post', 'pk', 'created_at', 'user']
