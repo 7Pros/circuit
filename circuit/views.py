b704817704f3a4f39ee5df895a7e35d65994389c
Matthias Loibl <me@metalmatze.de>
2015-07-07 19:27:35 +0200

    Improve readability of circuit/views.py

--- circuit/views.py
+++ circuit/views.py
@@ -9,5 +9,7 @@ from django.views.generic import TemplateView
 
 class LandingPage(TemplateView):
     template_name = 'landingpage.html'
+
+
 class Impressum(TemplateView):
     template_name = 'Impressum.html'
