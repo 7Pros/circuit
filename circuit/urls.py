5a0648655ae7943442caecebf945013b44eb7919
Gjum <gjum42@gmail.com>
2015-07-08 02:42:21 +0200

    Remove trailing slash from search URL

--- circuit/urls.py
+++ circuit/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^$', views.LandingPage.as_view(), name='landingpage'),
     url(r'^circles/', include('circles.urls', namespace='circles')),
-    url(r'^search/$', views.SearchView.as_view(), name='search'),
+    url(r'^search$', views.SearchView.as_view(), name='search'),
     url(r'^legal-notice/', views.LegalNotice.as_view(), name='legal-notice'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
